f864fb352b8d092a8e9099fd7787d90e2e875009
HellenG <hellen.gakuruh@gmail.com>
2017-01-13 08:18:32 +0300

    Added readMatch function

--- myGoogler.R
+++ myGoogler.R
@@ -1,3 +1,11 @@
 look.for <- function(pattern, directory = ".", ...){
    list.files(path = directory, pattern = pattern, all.files = TRUE, full.names = TRUE, recursive = TRUE, ignore.case = TRUE)
 }
+
+matchPattern <- function(x, pattern, ...){
+   n <- length(x)
+   lapply(1:n, function(i) {
+      content <- readLines(x[i])
+      grep(pattern, content, ...)
+   })
+}
