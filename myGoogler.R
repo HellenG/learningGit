193469ba890945b113201f8456ed63172f78fcb0
HellenG <hellen.gakuruh@gmail.com>
2017-01-13 08:42:17 +0300

    Changed default for argument "directory" in function "look.for"

--- myGoogler.R
+++ myGoogler.R
@@ -1,3 +1,3 @@
-look.for <- function(pattern, directory = ".", ...){
+look.for <- function(pattern, directory = "~", ...){
    list.files(path = directory, pattern = pattern, all.files = TRUE, full.names = TRUE, recursive = TRUE, ignore.case = TRUE)
 }
